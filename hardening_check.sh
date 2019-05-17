fce1878083afa132eae10b7d145423e3cb733deb
Steve Bartlett <50664843+ayethatsright@users.noreply.github.com>
2019-05-17 09:47:01 +0100

    Update hardening_check.sh

--- hardening_check.sh
+++ hardening_check.sh
@@ -1,1 +1,5 @@
+#! /bin/bash
+
+# This script checks the machine against all the settings in the 'DRAFT NIST SP 800-179r1 macOS 10.12 Security Baseline' and reports on it's compliance
+# It outputs the results to ./check_results.txt
 
